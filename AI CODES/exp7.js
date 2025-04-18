7a080cd6c511ae654f52bc0a78484253d39e2f5f
bloodyCoder93 <bloodygamer205@gmail.com>
2025-04-18 23:32:00 +0530

    Delete AI CODES directory

--- AI CODES/exp7.js
+++
@@ -1,9 +0,0 @@
-//exp7 : Resolution
-// Pseudo-representation since actual logic requires a theorem prover
-let clauses = ["A v B", "~A", "~B"];
-function resolution(clauses) {
-    console.log("Clauses:");
-    clauses.forEach(c => console.log(c));
-    console.log("Resolved: ⊥ (Contradiction)");
-}
-resolution(clauses);
